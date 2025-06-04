255b4764e120fae83f597c6bf5db8c0a9ebf2725
MSJ2025 <msj2025@icloud.com>
2025-06-05 00:32:41 +0200

    Connect area_entered for bonuses

--- scenes/magnet.gd
+++ scenes/magnet.gd
@@ -4,8 +4,14 @@ signal collected(bonus_node)
 
 func _ready() -> void:
     connect("body_entered", Callable(self, "_on_body_entered"))
+    connect("area_entered", Callable(self, "_on_area_entered"))
 
 func _on_body_entered(body: Node) -> void:
     if body is CharacterBody2D:
+        emit_signal("collected", self)
+        queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+    if area.get_parent() is CharacterBody2D:
         emit_signal("collected", self)
         queue_free()
